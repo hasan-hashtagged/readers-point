f06186d092f3b15e90a3ddb942b382ff98674331
hj <hj@gmail.com>
2020-05-17 02:58:31 +0530

    Adding styles to page

--- index.php
+++ index.php
@@ -5,20 +5,25 @@
 <html>
   <head>
     <title>First</title>
+    <link rel="stylesheet" type="text/css" href="css/main.css"/>
   </head>
   <body>
-      <div>
-        <ul>
-          <li><a href="signup.php">Register</a></li>
-          <li><a href="login.php">Login</a></li>
-        </ul>
-      </div>
-      <div>
+      <div class="topbar">
+        <span>
+          <ul>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="signup.php">Register</a></li>
+            <li><a href="login.php">Login</a></li>
+          </ul>
+        </span>
         <h2>Welcome to Readers' Point.</h2>
+      </div>
+      <div class="container">
         <h3>Books available</h3>
       </div>
       <div>
-        <table>
+        <table class="container" align="center">
           <?php
             if(mysqli_num_rows($result)>0) {
               while($row=mysqli_fetch_array($result)) {
